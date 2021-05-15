b075f3883ade8bf306b056582ca7284527a1c3b8
Josh Klopfenstein <joshklop10@gmail.com>
2021-05-15 13:37:01 -0500

    Implement search, delete, succ, and pred

--- linked-list.c
+++ linked-list.c
@@ -12,33 +12,66 @@ struct List {
 };
 
 /*
- * insert
- * delete
- * search
- * pred
- * succ
  * reverse
  */
 
 struct List *new();
 void printlist(struct List *l);
-void insert(struct List *l, struct Node *n);
+void insert(struct List *l, int k);
+void freelist(struct List *l);
+void search(struct List *l, struct Node **n, int k);
+void delete(struct List *l, int k);
+int *pred(struct List *l, int k);
+int *succ(struct List *l, int k);
 
 int main(void) {
     struct List *l = new();
+    /* Test insert and search */
+    insert(l, 1);
+    insert(l, 2);
+    struct Node *n = NULL;
+    search(l, &n, 1);
+    assert(n->key == 1);
     printlist(l);
-    struct Node n1 = {NULL, 3};
-    insert(l, &n1);
-    struct Node n2 = {NULL, 4};
-    insert(l, &n2);
+
+    /* Test delete */
+    puts("delete(l, 1)");
+    delete(l, 1);
     printlist(l);
-    free(l);
+
+    /* Test pred */
+    int *x = pred(l, 2);
+    assert(x == NULL);
+    insert(l, 1);
+    x = pred(l, 2);
+    assert(*x == 1);
+
+    /* Test succ */
+    x = succ(l, 1);
+    assert(*x == 2);
+    delete(l, 2);
+    x = succ(l, 1);
+    assert(x == NULL);
+
+    freelist(l);
 }
 
 struct List *new() {
     struct List *l = malloc(sizeof(struct List));
+    if (l == NULL) exit(1);
     l->head = NULL;
     return l;
+}
+
+void freelist(struct List *l) {
+    struct Node *n = l->head;
+    struct Node *m = l->head;
+    while (n != NULL) {
+        m = n;
+        n = n->next;
+        free(m);
+    }
+    free(l);
 }
 
 void printlist(struct List *l) {
@@ -50,7 +83,51 @@ void printlist(struct List *l) {
     puts("");
 }
 
-void insert(struct List *l, struct Node *n) {
+void search(struct List *l, struct Node **n, int k) {
+    *n = l->head;
+    while (*n != NULL && (*n)->key != k)
+        *n = (*n)->next;
+}
+
+void insert(struct List *l, int k) {
+    struct Node *n = malloc(sizeof(struct Node));
+    if (n == NULL) exit(1);
+    n->key = k;
     n->next = l->head;
     l->head = n;
+}
+
+void delete(struct List *l, int k) {
+    struct Node *n = l->head;
+    struct Node *m = l->head;
+    while (n != NULL && n->key != k) {
+        m = n;
+        n = n->next;
+    }
+    if (n->key == k) {
+        m->next = n->next;
+        free(n);
+    }
+}
+
+int *pred(struct List *l, int k) {
+    struct Node *n = l->head;
+    struct Node *m = l->head;
+    while (n != NULL && n->key != k) {
+        m = n;
+        n = n->next;
+    }
+    if (n == NULL || n == l->head)
+        return NULL;
+    else
+        return &m->key;
+}
+
+int *succ(struct List *l, int k) {
+    struct Node *n;
+    search(l, &n, k);
+    if (n == NULL || n->next == NULL)
+        return NULL;
+    else
+        return &n->next->key;
 }
